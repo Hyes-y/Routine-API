7fae378908dfe711861dd7551414163ca8da60ea
Hyes-y <gptjs7970@naver.com>
2022-10-14 15:20:12 +0900

    feat: User(Account) 앱 초기 세팅 - django AbstractBaseUser를 이용하여 User모델 정의 - superuser 생성을 위해 forms.py, models.py 작성 - django admin 사용을 위해 admin.py 작성

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     # rest_framework
     'rest_framework',
     # local apps
-    # 'apps.account.apps.AccountConfig',
+    'apps.account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -91,7 +91,7 @@ DATABASES = {
 }
 
 # Auth model setting
-# AUTH_USER_MODEL = 'account.User'
+AUTH_USER_MODEL = 'account.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
