78c522b3675ff153ca6569f595d668f0a667f7d1
Hyes-y <gptjs7970@naver.com>
2022-10-14 15:53:30 +0900

    feat: Password Validator 구현 - 8자 이상이고, 문자, 숫자, 특수문자 반드시 포함하는 validator 작성(apps/account/validator.py) - settings/base.py 에 해당 validator 적용

--- config/settings/base.py
+++ config/settings/base.py
@@ -101,13 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'apps.account.validator.PasswordRuleValidator',
     },
 ]
 
