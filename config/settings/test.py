c34f85f6f01621ff8b900641de5a51e92ae314c4
Hyes-y <gptjs7970@naver.com>
2022-10-20 03:05:16 +0900

    feat: 테스트용 기본 db(sqlite3)를 사용하기 위한 settings.test 추가

--- config/settings/test.py
+++ config/settings/test.py
@@ -2,3 +2,12 @@
 Test Settings
 """
 from .base import *
+# From Djcrety (https://djecrety.ir/)
+SECRET_KEY = 'jll3)ve!+zmdf)wggf@b*0!^tb#)#*u(cyvj)0^)+ns89a(*t^'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
